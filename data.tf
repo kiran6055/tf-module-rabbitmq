86f0bf48b3a398cd5efd2687bca6f0bfd1796a98
kiran1616 <kirankumar7163@gmail.com>
2023-04-12 18:56:34 +0530

    updated script

--- data.tf
+++ data.tf
@@ -2,7 +2,6 @@ data "aws_kms_key" "key" {
   key_id = "alias/roboshop"
 }
 
-
 data "aws_ssm_parameter" "USER" {
   name = "${var.env}.rabbitmq.USER"
 }
--- main.tf
+++ main.tf
@@ -52,7 +52,6 @@ resource "aws_mq_broker" "rabbitmq" {
   encryption_options {
     use_aws_owned_key = false
     kms_key_id =data.aws_kms_key.key.arn
-
   }
 
   user {
