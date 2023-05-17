72fee82398680d408637f558d68acfcf8c236f5c
kiran1616 <kirankumar7163@gmail.com>
2023-05-17 18:04:58 +0530

    m

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/kirankumar7163/ansible-roboshop roboshop.yml -e ROLE_NAME=${component} -e env=${env} | tee /opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/kiran6055/ansible-roboshop roboshop.yml -e ROLE_NAME=${component} -e env=${env} | tee /opt/ansible.log
